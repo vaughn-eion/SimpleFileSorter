a8ba61fefce847e9366be173cceb170c08c15ca4
Eion Vaughn <108884474+vaughn-eion@users.noreply.github.com>
2023-02-23 21:29:13 -0500

    Add files via upload

---
+++ file_sorter.py
@@ -0,0 +1,14 @@
+import os, shutil
+
+# Path to files that need to be sorted
+path = os.getcwd()
+path += "\\File_Sorter\\Files\\"
+
+# Pull in files from the selected folder and determines if a folder is needed and sorts them accordingly
+for file in os.listdir(path):
+    ext_index = (file.index('.'))
+    ext = file[ext_index:]
+    if not os.path.exists(f'{path}\\{ext[1:]}_files\\'):
+        os.makedirs(f'{path}\\{ext[1:]}_files\\')
+
+    shutil.move(f'{path}\\{file}', f'{path}\\{ext[1:]}_files\\')
